8ca34e3aeac371d3843c70d675822bebc15a7196
Denis <denis.balashevich@gmail.com>
2016-07-17 19:22:54 +0300

    task3

---
+++ NumberOfWordsTask/WordsNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfWordsTask
+{
+    public class WordsNumber
+    {
+        public static int WordFrequency(string str)
+        {
+            if (ReferenceEquals(str, null))
+                throw new ArgumentNullException();
+            string[] strArr = ParseWord(str).Split(new char[] { ' ' });
+            return WordsNum(strArr);
+        }
+
+        private static string ParseWord(string str)
+        {
+            char[] ch = str.ToCharArray();
+            for (int i = 0; i < ch.Length; i++)
+            {
+                if (!char.IsLetter(ch[i]))
+                    ch[i] = ' ';
+            }
+            return new string(ch);
+        }
+
+        private static int WordsNum(string[] strArr)
+        {
+            int wordsNum = 0;
+            foreach (var s in strArr)
+            {
+                if (!string.IsNullOrEmpty(s))
+                {
+                    wordsNum++;
+                }
+            }
+            return wordsNum;
+        }
+    }
+}
---
+++ NumberOfWordsTaskTests/WordsNumberTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NumberOfWordsTask;
+namespace NumberOfWordsTaskTests
+{
+    [TestFixture]
+    public class WordsNumberTests
+    {
+        [TestCaseSource("GetParameters")]
+        public double WordsNumber_Tests(string x)
+        {
+            return WordsNumber.WordFrequency(x);
+        }
+        public IEnumerable<TestCaseData> GetParameters()
+        {
+            yield return new TestCaseData("    Hello8888 a!@#$%^^&*worlds.    claas").Returns(4);
+            yield return new TestCaseData("").Returns(0);
+            yield return new TestCaseData(null).Throws(typeof(ArgumentNullException));
+        }
+    }
+}
